922f7a75a586ea1b19406f3695d7a13d7549da41
Sebastián P. <james.pinto@unl.edu.ec>
2025-06-16 22:05:22 -0500

    Adelanto

---
+++ src/enums/Enumcarreraciclo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enums;
+
+/**
+ *
+ * @author James Pinto
+ */
+public enum Enumcarreraciclo {
+    CARRERAS, CICLOS;
+    /*CARRERAS("carreras", 1), CICLOS("ciclos", 2);
+        private String Tipo;
+        private int Valor;
+        private CarrerasCiclos (String Tipo, int Valor){
+            this.Tipo = Tipo; 
+       }
+
+        public String getTipo() {
+            return Tipo;
+        }
+
+        public int getValor() {
+            return Valor;
+        }*/
+}
